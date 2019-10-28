5ef22b96565eceaefdfeed444580971d80202f5a
Gerko Vink <g.vink@uu.nl>
2019-10-28 07:32:18 +0100

    initial commit for 2019

---
+++ Wk4/Background/An example archive that generates itself/Simulation conditions/Simulation_multivariate 80.R
@@ -0,0 +1,34 @@
+#set random seed
+set.seed(123)
+
+#create population data
+#Sigma <- matrix(c(10,9,9, 9, 10, 9, 6, 3,10),3,3)
+#Sigma=matrix(c(1,0.8,0.8, 0.8,1,0.8, 0.8,0.8,1),3,3)
+Sigma=matrix(c(1,0.1,0.1, 0.1,1,0.1, 0.1,0.1,1),3,3)
+population <- as.data.frame(mvrnorm(n=populationsize, c(1, 2, 3), Sigma))
+colnames(population) <- c("x", "ymis1", "ymis2")
+
+#run simulation
+SIM <- simulate.multivariate(population, nsim=nsim, m=5, mis.p=.8)
+
+#run evaluation
+pooled <- evaluate.impute(SIM)
+
+#print pooled output
+finite1 <- Reduce("+", pooled$finite1) / length(pooled$finite1)
+rubin1 <- Reduce("+", pooled$rubin1) / length(pooled$rubin1)
+finite2 <- Reduce("+", pooled$finite2) / length(pooled$finite2)
+rubin2 <- Reduce("+", pooled$rubin2) / length(pooled$rubin2)
+out80 <- rbind(finite1, rubin1, finite2, rubin2)
+
+#run evaluation Barnard & Rubin (1999)
+pooledBR <- evaluate.impute.BR(SIM)
+
+#print pooled output Barnard & Rubin (1999)
+finite1BR <- Reduce("+", pooledBR$finite1) / length(pooledBR$finite1)
+rubin1BR <- Reduce("+", pooledBR$rubin1) / length(pooledBR$rubin1)
+finite2BR <- Reduce("+", pooledBR$finite2) / length(pooledBR$finite2)
+rubin2BR <- Reduce("+", pooledBR$rubin2) / length(pooledBR$rubin2)
+out80BR <- rbind(finite1BR, rubin1BR, finite2BR, rubin2BR)
+
+save.image("Workspaces/Multivariate_sim80.RData")
